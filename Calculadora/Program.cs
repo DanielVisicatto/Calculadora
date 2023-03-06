bb019c5b80903e79ca19d2bcd4d2fa6b7d54ff2a
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 19:24:51 -0300

    Adicionadas a exibição de um Menu e as Funcoes na Calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,30 +1,85 @@
-﻿float n1, n2;
-
-Console.Write("Digite o valor para  N1: ");
-n1 = float.Parse(Console.ReadLine());
-
 ﻿double num1, num2;
 int opcao = 0;
 
-Console.Write("Digite o valor para  N2: ");
-n2 = float.Parse(Console.ReadLine());
+ReceberEntrada();
+Escolher();
 
-float soma = n1 + n2;
-float diferenca = n1 - n2;
-float produto = n1 * n2;
-float quociente;
-
-if (n2 != 0)
+void ReceberEntrada()
 {
-    quociente = n1 / n2;
+    Console.Write("Informe o primeiro número: ");
+    num1 = double.Parse(Console.ReadLine());
 
-    Console.Write("Resultado da soma = {0}\n" +
-    "Resultado da diferença = {1}\n" +
-    "Produto = {2}\n" +
-    "Quociente = {3}", soma, diferenca, produto, quociente);
-
+    Console.Write("Informe o segundo número: ");
+    num2 = double.Parse(Console.ReadLine());
 }
-else
+void Escolher()
 {
-    Console.WriteLine("Impossível divisão por ZERO!");
+    while (opcao >= 0 && opcao != 5)
+    {
+        Menu();
+        switch (opcao)
+        {
+            default:
+                Console.WriteLine("Opcao inválida");
+                break;
+
+            case 1:
+                Console.WriteLine("A soma desses números é: {0}", Somar());
+                break;
+            case 2:
+                Console.WriteLine("A diferenca desses números é: {0}", Subtrair());
+                break;
+            case 3:
+                Console.WriteLine("O produto desses números é: {0}", Multiplicar());
+                break;
+            case 4:
+                VerificarDivisaoPorZero();
+                if (num2 > 0)
+                    Console.WriteLine("O quociente desses números é: {0}", Dividir());
+                break;
+            case 5:
+                Console.WriteLine("Obrigado por ultilizar nossos serviços!");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
+int Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Selecione uma opcao desejada.");
+    Console.WriteLine("1 - Somar\n" +
+                      "2 - Subtrair\n" +
+                      "3 - Multiplicar\n" +
+                      "4 - Dividir\n" +
+                      "5 - Sair;");
+    return opcao = int.Parse(Console.ReadLine());
+}
+double Somar()
+{
+    double resultado = num1 + num2;
+    return resultado;
+}
+double Subtrair()
+{
+    double resultado = num1 - num2;
+    return resultado;
+}
+double Multiplicar()
+{
+    double resultado = num1 * num2;
+    return resultado;
+}
+double Dividir()
+{
+    double resultado = num1 / num2;
+    return resultado;
+}
+void VerificarDivisaoPorZero()
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine("Impossível divisão por zero!");
+    }
+
 }
